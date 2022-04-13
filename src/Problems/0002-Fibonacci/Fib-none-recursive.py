0a8ed9a461c2b95d58391af402f6613dee4b8cc1
Pakan Talebi <pakantalebi2009@gmail.com>
2022-04-14 01:13:35 +0430

    nice

--- src/Problems/0002-Fibonacci/Fib-none-recursive.py
+++ src/Problems/0002-Fibonacci/Fib-none-recursive.py
@@ -1,22 +1,16 @@
-a = int(input('enter the number: '))
-list = []
-b = 0
-l = 0
-while True:
-    if l == 0:
-        b += 1
-        print(b)
-    if l == 1:
-        b += 1
-        print(b)
-        list.append(b)
-    if l != 0 or 1:
-        for x in list:
-            b += x
-            list.pop(0)
-        list.append(b)
-        if b < a:
-            print(b)
-    l += 1
-    if b >  a:
-        break
+c = int(input('enter the numebr: '))
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    else:
+        return fibonacci(n -1) + fibonacci(n -2)
+
+a = fibonacci (c)
+y = 1
+while y < c:
+    a = fibonacci (y)
+    print(a)
+    y += 1
