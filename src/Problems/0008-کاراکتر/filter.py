40b8e209b970d3ce0abf2e54ff21033c0128c8f4
Pakan Talebi <pakantalebi2009@gmail.com>
2022-05-09 23:07:11 +0430

    best

--- src/Problems/0008-کاراکتر/filter.py
+++ src/Problems/0008-کاراکتر/filter.py
@@ -1,17 +1,25 @@
-list1 = []
-def name():
+def getNames():
+    list1 = []
     while True:
         a = input('enter the name :')
         if a=='END':
-            print(list1)
             break
-        line = a.split(' ')
+        line = a.split(',')
         for x in line:
             list1.append(x)
-name()
-def number():
+    return list1
+l2 = getNames()
+print(l2)
+def greet(list2):
     b= int(input('enter the number: '))
-    result=(list(filter(lambda i:len(i)<b,list1)))
-    print(list(map(lambda u:f'Hello {u}',result)))
-    print(result)
-number()
+    result=(list(filter(lambda i:len(i)<b,list2)))
+    return result
+l3 = greet(l2)
+def hello(result1):
+    jack = list(map(lambda u:f'Hello {u}',result1))
+    print(jack)
+    return jack
+l4 = hello(l3)
+def membersoflist(list4):
+    print(list4)
+membersoflist(l3)
