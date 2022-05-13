99e1ca5550d87d0d8b0b0b0fb6545f11c8882475
Pakan Talebi <pakantalebi2009@gmail.com>
2022-05-13 16:28:12 +0430

    index

---
+++ src/Problems/0009-index/h,;d.py
@@ -0,0 +1,18 @@
+list = []
+list1 = []
+while True:
+    b = input()
+    if b == 'END':
+        break
+    line = b.split('.')
+    for i in line:
+        list.append(i)
+while True:
+    a = input()
+    if a == 'END':
+        break
+    list1.append(a)
+for x in list1:
+    for o in list:
+        if x in o:
+            print(o)
